823e2eb9f6288b15c7df8250bd2f14fde1f6a11b
Netinhoalves <vila.alves.pc@gmail.com>
2023-08-08 20:25:39 -0400

    aula 1

---
+++ aula-01/exercicio-01.js
@@ -0,0 +1,21 @@
+let a = 10;
+
+console.log(a);
+
+// let b = 12;
+
+// if (b ) {
+//   console.log('par');
+// } else {
+//   console.log('ímpar');
+// }
+
+var c = 7;
+var d = 5;
+var e = 3;
+
+if (c < 10 && d != 0 && e > 10) {
+  console.log(c, 'é menor que 10');
+  console.log(d, 'não é 0');
+  console.log(e, 'é maior que 10');
+}
